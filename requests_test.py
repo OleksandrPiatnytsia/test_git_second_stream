d314037405044694ab2b889dffa21dae4d46b058
Oleksandr Piatnytsia <a_pyatnytsia@viyar.ua>
2026-07-10 15:56:56 +0300

    ERP2-9999: url change

--- requests_test.py
+++ requests_test.py
@@ -5,7 +5,9 @@ import requests
 
 def main():
 
-    resp = requests.get(url="https://russianwarship.rip/api/v2/statistics?offset=0&limit=50&date_from=2022-02-24&date_to=2022-03-01")
+    url = "https://russianwarship.rip/api/v2/statistics/latest"
+
+    resp = requests.get(url=url)
 
     pprint(resp.json())
 
