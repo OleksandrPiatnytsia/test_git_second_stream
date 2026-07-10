30fa470cfcae5576ade6a8a54744e4c358b49ac8
Oleksandr Piatnytsia <a_pyatnytsia@viyar.ua>
2026-07-10 16:19:34 +0300

    ERP2-9999: some fixex

--- requests_test.py
+++ requests_test.py
@@ -1,13 +1,14 @@
 from pprint import pprint
+import datetime
 
 import requests
 
 
 def main():
 
-    print("Latest rassia looses:")
+    print(f"Latest rassia looses on date {datetime.date.today()}:")
 
-    url = "https://russianwarship.rip/api/v2/statistics/latest"
+    url = "https://russianwarship.rip/api/v2/statistics/latest/"
 
     resp = requests.get(url=url)
     print(f"response status_code{resp.status_code}")
