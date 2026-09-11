f25091a729013d5e17723a9e2844c451c9ee2e83
Oleksandr Piatnytsia <a_pyatnytsia@viyar.ua>
2026-09-11 16:17:08 +0300

    test print

--- requests_test.py
+++ requests_test.py
@@ -12,8 +12,9 @@ def main():
 
     resp = requests.get(url=url)
     print(f"response status_code{resp.status_code}")
-    pprint(resp.json().get("data").get("increase"))
+    pprint(f"unit losses: {resp.json().get("data").get("increase")}")
 
+    print("end")
 
 if __name__ == "__main__":
     main()
