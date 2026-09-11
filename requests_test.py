782ea9ef01c504d4619b77faa0d53a81e0312e04
Oleksandr Piatnytsia <a_pyatnytsia@viyar.ua>
2026-09-11 16:41:40 +0300

    test commit -am

--- requests_test.py
+++ requests_test.py
@@ -11,6 +11,9 @@ def main():
     url = "https://russianwarship.rip/api/v2/statistics/latest"
 
     resp = requests.get(url=url)
+
+
+
     print(f"response status_code{resp.status_code}")
     # pprint(resp.json().get("data").get("increase"))
     print("test")
