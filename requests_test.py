7c86d346be2ec58f534a55c1775a952087cb0a9e
Oleksandr Piatnytsia <o.piatnytsia@viyar.tech>
2026-09-18 15:44:30 +0300

    general fixes

--- requests_test.py
+++ requests_test.py
@@ -6,17 +6,14 @@ import requests
 
 def main():
 
-    print(f"Latest rassia looses on date {datetime.date.today()}:")
+    print(f"Latest russia looses on date {datetime.date.today()}:")
 
     url = "https://russianwarship.rip/api/v2/statistics/latest"
 
     resp = requests.get(url=url)
 
-
-
     print(f"response status_code{resp.status_code}")
-    # pprint(resp.json().get("data").get("increase"))
-    print("test")
+    pprint(resp.json().get("data").get("increase"))
 
 if __name__ == "__main__":
     main()
