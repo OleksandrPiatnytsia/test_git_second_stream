8fc1d2803255892cb0c6de185a0ada5f198f265e
Oleksandr Piatnytsia <o.piatnytsia@viyar.tech>
2026-09-18 15:50:11 +0300

    some fixes

--- requests_test.py
+++ requests_test.py
@@ -12,11 +12,8 @@ def main():
 
     resp = requests.get(url=url)
 
-
-
     print(f"response status_code{resp.status_code}")
-    # pprint(resp.json().get("data").get("increase"))
-    print("test")
+    pprint(resp.json().get("data"))
 
 if __name__ == "__main__":
     main()
